6911e8cd9f95a31d167204203f055fbb5245fab3
NavyGhost1942 <riverawilliam256@gmail.com>
2023-11-25 17:30:51 -0600

    commit

--- src/routes/api/index.js
+++ src/routes/api/index.js
@@ -3,6 +3,8 @@ import { Router } from 'express'
 
 import persons from './persons'
 import companies from './companies'
+import departments from './departments'
+import employees from './employees'
 
 const router = Router()
 
@@ -12,5 +14,7 @@ router.get('/api', (req, res) => {
 
 router.use('/persons', persons)
 router.use('/companies', companies)
+router.use('/departments', departments)
+router.use('/employees', employees)
 
 export default router
